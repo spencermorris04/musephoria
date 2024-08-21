627661b2a2a472ef910bed3707fdf276833d5090
Spencer Morris <spencerjonmorris@gmail.com>
2024-08-21 02:59:38 -0400

    added eslint bypasses for auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "~/server/auth";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 const handler = NextAuth(authOptions);
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export { handler as GET, handler as POST };
